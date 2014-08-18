3cd047693ee32510611f83fe1f7d5e5366b0f5d4
Nick Miloscia <njm5586@g.rit.edu>
2014-08-17 22:57:03 -0400

    Module 4
    Module 4 - adding $log + $interval directions, a controller, and custom
    directives.

--- script.js
+++ script.js
@@ -1,28 +1,56 @@
 // Code goes here
 
-var MainController = function($scope, $http) {
+(function() {
 
+  var app = angular.module('githubViewer', []);
+  var MainController = function($scope, github, $interval, $log, $anchorScroll,
+    $location) {
 
-  $scope.search = function(username) {
-    $http.get("https://api.github.com/users/" + username)
-      .then(onUserSubmit, errorFunction);
+    $scope.search = function(username) {
+      $log.info('Searching for: ' + username);
+      github.getUser(username).then(onUserSubmit, errorFunction);
+      if (countdownInterval) {
+        $interval.cancel(countdownInterval);
+        $scope.countdown = '';
+      }
+    };
+
+    var onRepos = function(data) {
+      $scope.repos = data;
+      $location.hash('userDetails');
+      $anchorScroll();
+    };
+
+    var errorFunction = function(response) {
+      $scope.error = 'idk it broke';
+      $scope.user = '';
+    };
+
+    var countdownInterval = null;
+    var startCountdown = function() {
+      countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+    };
+
+    var decrementCountdown = function() {
+      $scope.countdown--;
+      if ($scope.countdown < 1) {
+        $scope.search($scope.username);
+      }
+    };
+
+    var onUserSubmit = function(data) {
+      $scope.user = data;
+      $scope.error = '';
+      github.getRepos($scope.user).then(onRepos, errorFunction);
+    };
+
+    $scope.username = 'Angular';
+    $scope.message = 'Github Viewer';
+    $scope.repoSortOrder = '-stargazers_count';
+    $scope.countdown = 5;
+    startCountdown();
+
   };
-  var onRepos = function(response) {
-    console.log('called');
-    $scope.repos = response.data;
-  };
-  
-  var errorFunction = function (response){
-    $scope.error = 'idk it broke';
-    $scope.user = '';
-  };
-  
-  var onUserSubmit = function(response){
-    $scope.user = response.data;
-    $scope.error = '';
-    $http.get($scope.user.repos_url).then(onRepos, errorFunction);
-  };
-  
-  $scope.message = 'Github Viewer';
-  $scope.repoSortOrder = '-stargazers_count';
-};
+
+  app.controller('MainController', MainController);
+}());
